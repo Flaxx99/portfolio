6e825ce798df07cffb0e479c0d7ea22be7fb0ca4
Flaxx99 <luis.vidal.b90@gmail.com>
2025-01-26 16:39:09 -0600

    Portfolio terminado

---
+++ src/const/const.js
@@ -0,0 +1,80 @@
+export const studies = [
+  {
+    title: "TSU en desarrollo de software",
+    corporation: "Universidad Tecnológica de Chihuahua",
+    date: "09/2020 – 12/2023",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  astro: "astro",
+  tailwind: "tailwind",
+  git: "git",
+  github: "github",
+  mongodb: "mongodb",
+  vite: "vite",
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Vite",
+    icon: IconKeys.vite,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/sistema-reportes.webp",
+    title: "Sistema Captura Reportes",
+    link: "",
+    description: `Proyecto web para captura de reportes de producción en la empresa Genesis Industrial Group.`,
+  },
+  {
+    img: "../../../img/smart-tank.webp",
+    title: "Smart Tank",
+    link: "",
+    description: `Aplicación multiplataforma desarrollada en Flutter para el control y gestión eficiente del agua en tinacos.`,
+  },
+  {
+    img: "../../../img/dragonfly.webp",
+    title: "Dragonfly CUU",
+    link: "",
+    description: `Dragonfly Cuu es una página web desarrollada con tecnologías Vanilla (HTML, CSS, JavaScript y PHP). `,
+  },
+];
